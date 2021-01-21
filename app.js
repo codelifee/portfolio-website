6dce9f19ca14ecc3d931aaf9416a16fd814fbab3
Sumin Jung <suminjung@Sumins-Mac-mini.local>
2021-01-21 16:28:09 +0900

    fix: refactor each pages made each pages simplier

---
+++ app.js
@@ -0,0 +1,12 @@
+const git = document.querySelector(".git");
+const blog = document.querySelector(".blog");
+const youtube = document.querySelector(".youtube");
+
+const slide1 = document.querySelector(".slide-1");
+const slide2 = document.querySelector(".slide-2");
+const slide3 = document.querySelector(".slide-3");
+
+git.addEventListener("mouseover", () => {
+    console.log("hello");
+} )
+
